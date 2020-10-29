71059363f63f2967c3cbe65b1c61fbbfc8ff768f
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-10-29 18:02:29 -0400

    initial home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import Checkout from './components/Checkout';
+
+import React from 'react';
+import Home from "./pages/Home";
+
+import {HashRouter as Router, Route} from 'react-router-dom';
+
+// import Checkout from './components/Checkout';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+    </Router>
+  </div>
   );
 }
 
---
+++ client/src/components/typewriter-item.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Item(props) {
+  return (
+      <span className={"n"}>{props.charItem}</span>
+  );
+}
+export default Item;
