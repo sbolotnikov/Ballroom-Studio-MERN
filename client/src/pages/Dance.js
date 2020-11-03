ca46a815fe8143626fb22c730dfc6a1afec55d90
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-11-03 18:20:20 -0500

    added dance inspire page

--- client/src/pages/Dance.js
+++ client/src/pages/Dance.js
@@ -14,7 +14,7 @@ function DanceInspire(){
                     <img className="masthead-avatar mb-5 rounded-circle" src="assets/img/me.jpg" alt="myself"/>
                     
                     <h1 className="masthead-heading mb-0 text-dark">We are continously inspired by Excellence in the field of 
-                    danc. Here you can find links for  some of our colloaborators and biggest inspirations in the "World of Dance."</h1>
+                    danc. Here you can find links for  some of our collaborators and biggest inspirations in the "World of Dance."</h1>
                 </div>
             </header>
 
@@ -33,28 +33,29 @@ function DanceInspire(){
                         
                     <div className="col-md-6 col-lg-4 mb-5">
                             <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
-                                <Link to="/pwr" className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <Link to="https://www.shobanajeyasingh.co.uk/" className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                     <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
                                 </Link>
-                                <img className="img-fluid" src="assets/img/portfolio/powernine.png" alt="Card Trading App"/>
+                                <img className="img-fluid" src="client/public/ShobanaDance.png" alt="shobana dance site"/>
+                            </div>
+                        </div>
+                        
+
+                        <div className="col-md-6 col-lg-4 mb-5">
+                            <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
+                                <Link to="https://www.rambert.org.uk/" className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
+                                </Link>
+                                <img className="img-fluid" src="client/public/RambertDance.PNG" alt="rambert dance site"/>
                             </div>
                         </div>
 
                         <div className="col-md-6 col-lg-4 mb-5">
                             <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
-                                <Link to="/rt" className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                                <Link to="https://www.pineapple.uk.com/" className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                                     <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
                                 </Link>
-                                <img className="img-fluid" src="assets/img/portfolio/repairtracker.png" alt="repairTracker"/>
-                            </div>
-                        </div>
-
-                        <div className="col-md-6 col-lg-4 mb-5">
-                            <div className="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
-                                <Link to="/fsf" className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-plus fa-3x"></i></div>
-                                </Link>
-                                <img className="img-fluid" src="assets/img/portfolio/fsfitness.png" alt="Fitness App"/>
+                                <img className="img-fluid" src="client/public/PineappleDance.png" alt="Pineapple dance site"/>
                             </div>
                         </div>
 
