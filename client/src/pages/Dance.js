d093493065f17a6858234b97611a57e0d1e55e21
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-11-05 10:17:09 -0500

    ádded icon

--- client/src/pages/Dance.js
+++ client/src/pages/Dance.js
@@ -27,7 +27,7 @@ function DanceInspire() {
          <div className='divider-custom'>
            <div className='divider-custom-line'></div>
            <div className='divider-custom-icon'>
-             <i className='fas fa-book-open'></i>
+             <i className='fas fa-globe'></i>
            </div>
            <div className='divider-custom-line'></div>
          </div>
